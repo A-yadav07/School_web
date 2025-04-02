a8b61152b5f3e9969faa10a254a26a8ab30303e0
A-yadav07 <ayadav02072001@gmail.com>
2025-04-02 14:15:18 +0530

    Updated Footer component

--- School_web/src/Component/Footer.jsx
+++ School_web/src/Component/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+    return (
+      <footer className="bg-gray-900 text-white py-6">
+        <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
+          <div className="text-center md:text-left">
+            <h2 className="text-xl font-bold">Astha</h2>
+            <p className="text-sm text-gray-400">&copy; {new Date().getFullYear()} Anand. All rights reserved.</p>
+          </div>
+          <nav className="mt-4 md:mt-0">
+            <ul className="flex space-x-6">
+              <li><a href="#" className="hover:text-gray-400">Home</a></li>
+              <li><a href="#" className="hover:text-gray-400">About</a></li>
+              <li><a href="#" className="hover:text-gray-400">Services</a></li>
+              <li><a href="#" className="hover:text-gray-400">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
