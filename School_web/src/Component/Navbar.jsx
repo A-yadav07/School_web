ab1023aabfe542d65ca173acdafdc90d86c74913
abhilasha1235 <abhilashap868@gmail.com>
2025-04-02 16:10:54 +0530

    change

--- School_web/src/Component/Navbar.jsx
+++ School_web/src/Component/Navbar.jsx
@@ -4,20 +4,26 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-red-500 text-white px-6 md:px-20 py-4 fixed w-full top-0 shadow-lg">
+    <nav className="bg-yellow-950 text-white  md:px-20 py-4 left-0 fixed w-full top-0 shadow-lg">
       <div className="flex justify-between items-center">
         {/* Logo */}
-        <h1 className="text-2xl font-bold text-blue-400">MyPortfolio</h1>
+        <h1 className="text-2xl font-bold text-white">MySchool</h1>
 
         {/* Desktop Menu */}
-        <ul className="hidden md:flex space-x-6">
+        <ul className="hidden md:flex space-x-12 mr-5 font-medium">
           <li><a href="#home" className="hover:text-blue-400">Home</a></li>
           <li><a href="#about" className="hover:text-blue-400">About</a></li>
           <li><a href="#projects" className="hover:text-blue-400">Projects</a></li>
-          <li><a href="#contact" className="hover:text-blue-400">Contact</a></li>
-        </ul>
+          <li><a href="#contact" className="hover:text-blue-400">Gallery</a></li>
+          <li><a href="#contact" className="hover:text-blue-400">Classes</a></li>
+          <li><a href="#contact" className="hover:text-blue-400">Education</a></li>
 
-        {/* Mobile Menu Button */}
+          <li><a href="#contact" className="hover:text-blue-400">Contact</a></li>
+
+        </ul>
+        <button className="bg-amber-50 text-black px-5 py-1 rounded-md font-medium"> Call Now</button>
+
+        {/* Mobile Menu Button */} 
         <button onClick={() => setIsOpen(!isOpen)} className="md:hidden">
           {isOpen ? "✖" : "☰"}
         </button>
